a32b9891e68c6e9d5bbdd4f6fb1289d12cb99eb2
websupergirl <nikol@thatsupergirl.com>
2022-10-23 06:55:13 -0700

    add option for admin url

--- functions.php
+++ functions.php
@@ -29,8 +29,10 @@ function medusa_admin_menu(){
 }
  
 function medusa_admin_menu_callback(){
-	echo '<iframe src="https://playful-faloodeh-d031e6.netlify.app/" title"MedusaJS Admin" style="padding:0; margin:0; width:100%; height:100vh;top:0px;left:0px;right:0px;bottom:0px;display:block;"></iframe>';
+	echo '<iframe src="' . get_option( 'admin_url' ) . '" title"MedusaJS Admin" style="padding:0; margin:0; width:100%; height:100vh;top:0px;left:0px;right:0px;bottom:0px;display:block;"></iframe>';
 }
+
+// add submenu for settings 
 
 add_action( 'admin_menu', 'medusa_submenu' );
 
@@ -48,5 +50,54 @@ function medusa_submenu(){
 }
 
 function medusa_admin_settings_callback(){
-	echo 'What is up?';
+	?>
+		<div class="wrap">
+			<form method="post" action="options.php">
+				<?php
+					settings_fields( 'medusa_store_settings' );
+					do_settings_sections( 'medusa_settings' );
+					submit_button();
+				?>
+			</form>
+		</div>
+	<?php
+}
+
+// register options
+
+add_action( 'admin_init',  'medusa_settings_fields' );
+
+function medusa_settings_fields(){
+
+	$page_slug = 'medusa_settings';
+	$option_group = 'medusa_store_settings';
+
+	add_settings_section(
+		'medusa_section_id',
+		'MedusaJS Admin URL',
+		'',
+		$page_slug
+	);
+
+	register_setting( $option_group, 'admin_url', '' );
+
+	// 3. add fields
+	add_settings_field(
+		'admin_url',
+		'Admin URL',
+		'medusa_input',
+		$page_slug,
+		'medusa_section_id'
+	);
+
+}
+
+function medusa_input( $args ) {
+	$value = get_option( 'admin_url' );
+	?>
+		<label>
+      What is the url of your Medusa JS Admin?
+    </label>
+		<input type="text" name="admin_url" value="<?php echo $value; ?>" />
+	<?php
 }
